2123b1e61c5223e934a271a21d637dec48bcbcc9
albedo27 <aravindharvindh8610@gmail.com>
2026-07-08 08:40:19 +0530

    task 1

---
+++ web.html/ex.program/ex.js
@@ -0,0 +1,38 @@
+
+    for (counter=1;counter<=10;counter++){
+        console.log(counter)
+    }
+
+    for (counter=1;counter<=10;counter++){
+        console.log (11-counter)
+    }
+
+    for (counter=1;counter<=20;counter++){
+        if(counter%2==0){
+            console.log(counter)
+        }
+    }
+
+    for (counter=1;counter<=20;counter++){
+        if(counter%5==0){
+            console.log(counter / 5 + " x 5 = " + counter)
+        }
+    }
+        console.log(" ")
+
+    let num=5
+    let limit=5
+    for (counter=1;counter<=limit;counter++){
+        console.log(num + " x " + counter + " = " + (num*counter))
+
+    }
+
+    let sum=0
+    for (counter=1;counter<=100;counter++){
+        sum+=counter;
+    }
+    console.log("Sum of 1 to 100 is: " + sum)
+
+    
+    let sum=1
+    for(counter=1;counter<=10)
